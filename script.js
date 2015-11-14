52c2c62ffcfdef4eded5359ed07d24bc4225f057
Veren <sunyatasattva@gmail.com>
2015-11-15 00:35:18 +0100

    Find the difference between the played overtone and 12-TET and display it.

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 window.tones = require("./tones.js");
 
+var utils = require("./utils.js");
+
+var tTET = require('./12-tet.json');
+
 $(document).ready(function($){
     
   $('.overtone').on('click', function(){
       var baseFrequency = $('#base').val(),
           idx           = $(this).index() + 1;
       console.log(baseFrequency);
-      tones.playFrequency( idx * baseFrequency );
+      var tone = tones.playFrequency( idx * baseFrequency );
+      
+      // @todo obviously clean this up
+      var frequencies      = utils.values(tTET),
+          closestFrequency = utils.binarySearch( idx * baseFrequency, frequencies),
+          note             = utils.findKey( tTET, function(frequency){ return frequency === closestFrequency } ).split(/(\d)/),
+          centsDifference  = tone.intervalInCents( { frequency: closestFrequency } );
+      
+      $('#note-name').text(note[0]);
+      $('#note sub').text(note[1]);
+      $('#cents-difference')
+          .css('text-indent', centsDifference + "%")
+          .find('span')[0].innerHTML = centsDifference > 0 ? "+" + centsDifference : centsDifference;
+
+      $('#cent-bar').css('left', 50 + centsDifference / 2 + "%");
+      
+      console.log(note[0], closestFrequency, centsDifference);
   });
     
   $('.spiral-piece').on('click', function(){
