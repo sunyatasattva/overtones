c2391daaaa29ab2c040e0d872b409d27da43baa4
Veren <sunyatasattva@gmail.com>
2015-11-15 00:33:57 +0100

    Adding utility functions from lodash.
    Added `values` and `findKey`

--- utils.js
+++ utils.js
@@ -1,5 +1,29 @@
 module.exports = {
-    logBase: function(base,n){
+    logBase: function(base,n) {
         return Math.log(n) / Math.log(base);
-    }
+    },
+    binarySearch: function(target, array) {
+        var highestIndex = array.length - 1,
+            lowestIndex  = 0,
+            middlePoint;
+        
+        while(highestIndex - lowestIndex > 1) {
+            middlePoint = Math.floor( (lowestIndex + highestIndex) / 2);
+            
+            if(array[middlePoint] < target) {
+                lowestIndex = middlePoint;
+            } else {
+                highestIndex = middlePoint;
+            }
+        }
+
+        if( target - array[lowestIndex] <= array[highestIndex] - target ) {
+            return array[lowestIndex];
+        }
+        
+        return array[highestIndex];
+    },
+    
+    findKey: require("lodash.findkey"),
+    values:  require("lodash.values")
 }
