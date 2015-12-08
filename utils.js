8b19b6250b3017df20485c99c8c7cd8930e0292d
Veren <sunyatasattva@gmail.com>
2015-12-08 20:59:01 +0100

    Adds utility function to convert RGB colors to HEX.

--- utils.js
+++ utils.js
@@ -28,6 +28,15 @@ module.exports = {
     logBase: function(base,n) {
         return Math.log(n) / Math.log(base);
     },
+    rgbToHex: function(rgb) {
+        if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        function hex(x) {
+            return ( "0" + parseInt(x).toString(16) ).slice(-2);
+        }
+        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+    },
     
     debounce: require("lodash.debounce"),
     findKey:  require("lodash.findkey"),
