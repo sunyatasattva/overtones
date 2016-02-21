65256789be58c31604a4086ff1c90654b7351436
Veren <sunyatasattva@gmail.com>
2016-02-21 19:15:55 +0100

    Adding deploy gulp task and favicons

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
+require('dotenv').load();
+
 var gulp = require('gulp');
+var gutil = require('gutil');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 var include = require('gulp-include');
 var minifyHTML = require('gulp-htmlmin');
 var minifySVG = require('gulp-svgmin');
+var decompress = require('gulp-decompress');
 
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+
+var ftp = require( 'vinyl-ftp' );
+
+gulp.task('assets', function() {
+    return gulp.src('./favicons.zip')
+        .pipe( decompress() )
+        .pipe( gulp.dest('build/') )
+});
  
 gulp.task('css', function () {
   return gulp.src('./assets/styles/*.scss')
@@ -36,6 +48,18 @@ gulp.task('javascript:dev', function () {
     debug: true
   });
 
+  var webfont = browserify({
+    entries: './assets/js/webfont.js',
+    debug: true
+  });
+    
+  webfont.bundle()
+    .pipe( source('./assets/js/webfont.js') )
+    .pipe( buffer() )
+    .pipe( sourcemaps.init({loadMaps: true}) )
+    .pipe( sourcemaps.write('./') )
+    .pipe( gulp.dest('./build/') );
+
   return b.bundle()
     .pipe( source('./assets/js/script.js') )
     .pipe( buffer() )
@@ -49,6 +73,19 @@ gulp.task('javascript', function () {
     entries: './assets/js/script.js',
     debug: true
   });
+    
+  var webfont = browserify({
+    entries: './assets/js/webfont.js',
+    debug: true
+  });
+    
+  webfont.bundle()
+    .pipe( source('./assets/js/webfont.js') )
+    .pipe( buffer() )
+    .pipe( sourcemaps.init({loadMaps: true}) )
+        .pipe( uglify() )
+    .pipe( sourcemaps.write('./') )
+    .pipe( gulp.dest('./build/') );
 
   return b.bundle()
     .pipe( source('./assets/js/script.js') )
@@ -66,3 +103,21 @@ gulp.task('watch', function() {
 });
 
 gulp.task('build', ['css', 'html', 'javascript']);
+
+gulp.task('deploy', ['build', 'ftp']);
+
+gulp.task('ftp', function () {
+
+    var conn = ftp.create( {
+        host:     process.env.FTP_HOST,
+        user:     process.env.FTP_USER,
+        password: process.env.FTP_PASS,
+        parallel: 10,
+        log:      gutil.log
+    } );
+
+    return gulp.src( 'build/**', { buffer: false } )
+        .pipe( conn.newer( '/asmi/overtones/deploy' ) ) // only upload newer files
+        .pipe( conn.dest( '/asmi/overtones/deploy' ) );
+
+});
