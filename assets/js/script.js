7921e43cbdca5cd58d56feea5db162ebe1f1ae82
Veren <sunyatasattva@gmail.com>
2017-07-25 00:21:35 +0200

    Removes browser class to html. For now it doesn't seem to be required.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ window.Tones     = require("./lib/tones");
 window.Overtones = require("./overtones");
 
 jQuery(document).ready(function($){
-	$("body").addClass(browser.name); // This makes me sad, it's 2016 Firefox!
-
 	tour.init();
 	window.Overtones.init();
 	analytics($);
